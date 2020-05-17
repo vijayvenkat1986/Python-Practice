8de4f80af8adfbd5b310783ea55967e278efa9b8
vijayvenkatesh1986 <vijayvenkatesh1986@gmail.com>
2020-05-17 13:54:11 +0530

    Initial commit

---
+++ escapechar.py
@@ -0,0 +1,20 @@
+splitSpring = "The Statement \nprints the line \nin seperate lines"
+print(splitSpring)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The per shop owner said "No, No, \'e  \'s uh,....he\'s resting".')
+print("The pet shop owner said \"No, No, 'e  's uh,....he's resting\".")
+print("""The per shop owner said "No, No, 'e  's uh,....he's resting".""")
+
+anotherSplitString  = """This String has been \
+split over \
+several strings \
+across \
+different lines."""
+
+print(anotherSplitString)
+
+
+
---
+++ strings.py
@@ -0,0 +1,11 @@
+print("Today is a good day to learn python")
+
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
+greeting = 'Hello'
+name = input('Please enter your name ')
+print(greeting + name)
+print(greeting+' '+name)
+
